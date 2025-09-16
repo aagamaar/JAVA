217e0716b19ffd215fcf777de264e12db21e1076
aagamaar <aagamaar01@gmail.com>
2025-09-16 22:29:21 +0530

    added userInput.java and class files

---
+++ userInput/userInput.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class userInput {
+    public static void main (String[]args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("What is your name?");
+        String name = sc.nextLine();
+        System.out.println("Hello " + name + ", have a nice day!");
+    }
+}
